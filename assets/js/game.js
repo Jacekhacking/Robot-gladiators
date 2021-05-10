72b5158e3b04f97b3762b132fac407aa688c783c
Jacek Hacking <jacekhacking@Jaceks-MacBook-Pro.local>
2021-05-10 11:42:54 -0600

    added-skip-penalty-and-confirm-prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 console.log(enemyName, enemyHealth, enemyAttack);
 
+var playerMoney = 10;
+
 var fight = function () {
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
@@ -44,9 +46,15 @@ var fight = function () {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
     } else if (promptFight === "skip" || promptFight === "SKIP") {
-        window.alert(playerName + " has chosen to skip the fight!");
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit? This will cost you 2 moneys");
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            playerMoney = playerMoney - 2;
+            console.log("money remaining " + playerMoney);
+        }
     } else {
-        window.alert("You need to choose a valid option. Try again!");
+        fight();
     }
 }
 fight();
